a08c1b9c5ceda7d342d22ca6dbb58c10e45cfd24
solrukas <solrukas@daegu.ac.kr>
2022-10-14 23:09:08 +0900

    Add node & express examples

---
+++ WEB(BE)/server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+//app.get('/', (req, res) => {
+//  res.send('Hello World');
+//});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/web/main.html');
+    })    
+
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
