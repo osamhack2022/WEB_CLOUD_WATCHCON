31227a28783919962ef2add1e39b2058db30c5af
solrukas <solrukas@daegu.ac.kr>
2022-10-16 16:02:31 +0900

    Add nodemon settings

--- WEB(BE)/server.js
+++ WEB(BE)/server.js
@@ -1,21 +1,28 @@
-'use strict';
-
 const express = require('express');
-
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
 const app = express();
-//app.get('/', (req, res) => {
-//  res.send('Hello World');
-//});
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/web/main.html');
-    })    
+app.use(express.json());
 
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/api/list', (req, res) => {
+  res.json([
+      { id: 'test1', name: 't1' },
+      { id: 'test2', name: 't2' },
+      { id: 'test3', name: 't3' },
+      { id: 'test4', name: 't4' },
+  ]);
 });
 
+app.get('/api/item', (req, res) => {
+  res.json({
+      id: 'test1',
+      name: 'tt1'
+  });
+});
+
+app.listen(8080, (err) => {
+  if (err) {
+      console.log('error', err);
+  }
+
+  console.log('8080 port listening');
+});
