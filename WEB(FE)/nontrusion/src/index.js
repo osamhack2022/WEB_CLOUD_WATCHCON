b7c50a214b84047ee4c0f74d1c628d61ff7a9509
영연 <cachi@kakao.com>
2022-10-25 13:41:25 +0000

    Bug Fix (ReactDOM.render is no longer supported in React 18)

--- WEB(FE)/nontrusion/src/index.js
+++ WEB(FE)/nontrusion/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
     <BrowserRouter>
         <App />
-	</BrowserRouter>,
-	document.getElementById('root')
+    </BrowserRouter>
 );
